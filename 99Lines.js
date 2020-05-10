50c973455ce9d09f02f4cb20b5485d78b44d9de0
Maggie Thompson <maggiethompson@Maggies-MacBook-Air.local>
2020-05-09 20:12:10 -0500

    completed lab

---
+++ 99Lines.js
@@ -0,0 +1,26 @@
+let friends = [ {
+    name: 'salem', 
+},
+{
+    name: 'junie',
+},
+{
+    name: 'ghost',
+},
+{
+    name: 'pants',
+},
+{
+    name:'marvin'
+}];
+friends.map(linesOfCode);
+
+function linesOfCode(friends) {
+    for (let i = 99;  i > 0;  i--) {
+         if (i !== 1) {
+             console.table( [i] + " Lines of code in the file, " + [i] + " lines of code, " + friends.name + " strikes ones out, clears it all out, " + [i - 1] + " lines of code in the file");
+         } else {
+             console.table("1 line of code in the file, 1 line of code," + friends.name + " strikes it out clears it all out, no more code in the file");
+         }
+     }
+ }; 
