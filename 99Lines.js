9173dfa9a69546bd4db96bb2188c58a87518661d
Maggie Thompson <maggiethompson@Maggies-MacBook-Air.local>
2020-05-12 12:40:43 -0500

    added a name to each output beginning

--- 99Lines.js
+++ 99Lines.js
@@ -18,9 +18,9 @@ friends.map(linesOfCode);
 function linesOfCode(friends) {
     for (let i = 99;  i > 0;  i--) {
          if (i !== 1) {
-             console.table( [i] + " Lines of code in the file, " + [i] + " lines of code, " + friends.name + " strikes ones out, clears it all out, " + [i - 1] + " lines of code in the file");
+             console.table(friends.name + ": "+ [i] + " Lines of code in the file, " + [i] + " lines of code, " + friends.name + " strikes ones out, clears it all out, " + [i - 1] + " lines of code in the file");
          } else {
-             console.table("1 line of code in the file, 1 line of code," + friends.name + " strikes it out clears it all out, no more code in the file");
+             console.table(friends.name + ": " + "1 line of code in the file, 1 line of code," + friends.name + " strikes it out clears it all out, no more code in the file");
          }
      }
  }; 
